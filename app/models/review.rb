396ed6e8bbeebe0709b447f6ce269a9d83d7de5b
dtsunekawa <dantsunekawa@gmail.com>
2012-11-07 21:13:45 -0800

    finished tags model, not sure how to do crud without reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,7 @@ class Review
   field :content,		:type => String
   belongs_to :user
   belongs_to :store
+  has_many :tags
 
   # This throws an error when creating a Review
   #references_many :tags
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,40 @@
 class Tag
   include Mongoid::Document
-
-
   	field :name, 			:type => String
 
-	referenced_in :review
+	belongs_to :review
+
+	# returns names of most popular tags
+	def most_popular(threshold)
+		hash = Tag.all_unique # returns a list of names sorted by popularity
+		names = Array.new
+
+		hash.each_with_index do |name, index|
+			if index >= threshold
+				return names
+			end
+
+			names << hash[name]
+		end
+	end
+
+	# returns a hash of all unique tag names and their counts
+	def all_unique
+		tag_list = Hash.new {}
+
+		all_tags = Tag.all
+
+		for all_tags do |t|
+			if tag_list.has_key( t.name )
+				tag_list[ t.name => 1 ]
+			else
+				tag_list[t.name] += 1;
+		end unless all_tags.nil?
+
+		# sort by value in descending order
+		tag_list.metrics.sort_by {|name, count| count}.reverse
+
+		return tag_list
+	end
+
 end
