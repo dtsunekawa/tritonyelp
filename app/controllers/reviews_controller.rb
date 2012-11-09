a8991c1d43054abd5d7d001f9dd85907b33bc503
Joshua <joshua@joshua-K52F.(none)>
2012-11-08 16:46:33 -0800

    Forgot to save one file

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+	def index
+		##Index shit goes here
+	end
+
+		##Create Review
+		def new
+			@review = review.new
+		end
+
+		##Read Review
+		def show
+		end
+
+		##Update Review
+		def edit
+			@review = review.find(params[:id])
+		end
+
+		##Destroy Review
+		def delete
+		end
+
 end
