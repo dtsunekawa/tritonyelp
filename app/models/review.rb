d80cb2193771c2597061b8a5bcb3c4745bd74ba7
Joshua <joshua@joshua-K52F.(none)>
2012-11-08 16:45:09 -0800

    Generated and edited reviews controller and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,17 @@
 class Review
   include Mongoid::Document
 
+  ##Content
   field :content,		:type => String
+ 
+  ##Trackable
+  field :created_at, :type => Time
+
   belongs_to :user
   belongs_to :store
   has_many :tags
+
+
 
   # This throws an error when creating a Review
   #references_many :tags
