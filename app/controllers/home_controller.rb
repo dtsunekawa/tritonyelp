6bd8008e20e1c0ae95bb9fc01701a3677b2fe4f4
John Sabilla <jsabilla@ucsd.edu>
2012-11-10 18:59:17 -0800

    Just added display user name and role in main page wrote role filters in user.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
 
 	def index
-		@name = current_user.name
-		@role = current_user.role
+		if(user_signed_in?)
+		    @name = current_user.name
+		    @role = current_user.role
+	    end
 	end
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
 
-
+  field :lname, :type => String
   field :name, :type => String
 	field :role, :type => String, :default => :customer
 
