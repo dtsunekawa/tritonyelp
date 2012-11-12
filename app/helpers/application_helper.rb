711d045f18db62f37717361691ef279774645d1b
ignition25 <cdjuarez@ucsd.edu>
2012-11-12 15:59:53 -0800

    Quick fix for authentication.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
-module ApplicationHelper
-
-#For Authentication, use a before_filter
-
-	def require_merchant
-		if(!user_signed_in? or (!current_user.is_merchant and !current_user.is_admin))
-			redirect_to root_path, :notice => 'You must be a merchant or administrator to use this action!'
-		end		
-	end
-
-	def require_admin
-		if(!user_signed_in? or (!current_user.is_admin))
-			redirect_to root_path, :notice => 'You must be an administrator to use this action!'
-		end			
-	end
-	
+module ApplicationHelper	
 end
