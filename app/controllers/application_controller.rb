b53be2c51d3c8a105409a5b5a951f5bdf6e152b1
ignition25 <cdjuarez@ucsd.edu>
2012-11-12 16:23:57 -0800

    Added styling for flash[:error] and flash[:notice].

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
 #For Authentication, use a before_filter
 
 	def require_merchant
-		if(!user_signed_in? or (!current_user.is_merchant and !current_user.is_admin))
-			redirect_to root_path, :notice => 'You must be a merchant or administrator to use this action!'
+		if(!user_signed_in? or (!current_user.is_merchant? and !current_user.is_admin?))
+			redirect_to root_path, :flash => { :error => 'You must be a merchant or administrator to use this action!' }
 		end		
 	end
 
 	def require_admin
-		if(!user_signed_in? or (!current_user.is_admin))
-			redirect_to root_path, :notice => 'You must be an administrator to use this action!'
+		if(!user_signed_in? or (!current_user.is_admin?))
+			redirect_to root_path, :flash => { :error => 'You must be an administrator to use this action!' }
 		end			
 	end
 end
