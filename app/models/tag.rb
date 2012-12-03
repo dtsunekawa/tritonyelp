0b5bd3b81eebcc3b528f7269823a4a9124c4d601
Sacamano <n.d.papps@gmail.com>
2012-12-03 11:18:44 -0800

    Fixed the seed file it should be good to go. Changed tags HABTM reviews, review HABTM tags, store HABTM tags, tag HABTM stores. HABTM must be bidirectional.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,8 +3,8 @@ class Tag
   	field :name, :type => String
   	field :popularity, :type => Integer, :default => 0
 
-	belongs_to :review
-	belongs_to :store
+	has_and_belongs_to_many :reviews
+	has_and_belongs_to_many :stores
 
 	def to_str
 		self.name
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,38 @@
 # bundle exec rake db:seed
 ###########################
 
-puts 'PURGING DATABASE'
+
+breakfast = '#breakfast'
+lunch = '#lunch'
+sandwich = '#sandwich'
+burrito = '#burrito'
+bagel = '#bagel'
+coffee = '#coffee'
+sandwich = '#sandwich'
+healthy = '#healthy'
+unhealthy = '#unhealthy'
+gross = '#gross'
+expensive = '#expensive'
+inexpensive = '#inexpensive'
+water_refill = '#water_refill'
+free = '#free'
+chicken = '#chicken'
+asian = '#Asian'
+ibs = '#IBS'
+burger = '#burger'
+american = '#American'
+stingy = '#stingy'
+indian = '#indian'
+snacks = '#snacks'
+drinks = '#drinks'
+convenient = '#convenient'
+
+puts 'Purging database...'
 Mongoid.purge!
 
-puts 'creating admin...'
+puts 'Creating admin...'
 admin = User.create!(name: 'admin', email: 'admin@ucsd.edu', lname: 'admin', role: 'admin', password: 'password')
+puts 'Admin ' << admin.name << ' ' << admin.lname << ' created.'
 
 puts 'Creating merchants...'
 
@@ -33,11 +60,11 @@ puts 'Merchant ' << goodys_merchant.name << ' created.'
 panda_merchant = User.create! :name => 'Ray', :lname => 'Rice', :role => 'merchant', :email => 'rrice@gmail.com', :password => 'password', :password_confirmation => 'password'
 puts 'Merchant ' << panda_merchant.name << ' created.'
 
-subway_merchant = User.create! :name => 'Torrey Smith', :lname => 'Smith', :role => 'merchant', :email => 'tsmith@gmail.com', :password => 'password', :password_confirmation => 'password'
+subway_merchant = User.create! :name => 'Torrey', :lname => 'Smith', :role => 'merchant', :email => 'tsmith@gmail.com', :password => 'password', :password_confirmation => 'password'
 puts 'Merchant ' << subway_merchant.name << ' created.'
 
 bombay_merchant = User.create! :name => 'Cecil', :lname => 'Shorts', :role => 'merchant', :email => 'cshorts@gmail.com', :password => 'password', :password_confirmation => 'password'
-puts 'Merchant ' << cups_merchant.name << ' created.'
+puts 'Merchant ' << bombay_merchant.name << ' created.'
 
 bk_merchant = User.create! :name => 'Brandon', :lname => 'Lloyd', :role => 'merchant', :email => 'blloyd@gmail.com', :password => 'password', :password_confirmation => 'password'
 puts 'Merchant ' << bk_merchant.name << ' created.'
@@ -46,7 +73,7 @@ sunshine_merchant = User.create! :name => 'Doug', :lname => 'Martin', :role => '
 puts 'Merchant ' << sunshine_merchant.name << ' created.'
 
 
-puts 'creating customers...'
+puts 'Creating customers...'
 
 nico_p = User.create(name: 'Nico', lname: 'Pappagianis', email: 'nico@customer.com', role: 'customer', password: 'password')
 puts 'Customer ' << nico_p.name << ' ' << nico_p.lname << ' created.'
@@ -61,7 +88,7 @@ john_s = User.create(name: 'John', lname: 'Sabilla', email: 'john@customer.com',
 puts 'Customer ' << john_s.name << ' ' << john_s.lname << ' created.'
 
 tuan_b = User.create(name: 'Tuan', lname: 'Bui', email: 'tuan@customer.com', role: 'customer', password: 'password')
-puts 'Customer ' << john_s.name << ' ' << john_s.lname << ' created.'
+puts 'Customer ' << tuan_b.name << ' ' << tuan_b.lname << ' created.'
 
 matt_n = User.create(name: 'Matt', lname: 'Nyugen', email: 'matt@customer.com', role: 'customer', password: 'password')
 puts 'Customer ' << matt_n.name << ' ' << matt_n.lname << ' created.'
@@ -75,18 +102,7 @@ puts 'Customer ' << ingolf_k.name << ' ' << ingolf_k.lname << ' created.'
 jack_b = User.create(name: 'Jack', lname: 'Bauer', email: 'jack@customer.com', role: 'customer', password: 'password')
 puts 'Customer ' << jack_b.name << ' ' << jack_b.lname << ' created.'
 
-
-u_merchant5 = User.create! :name => 'TritonYelp', :lname => 'Merchant', :role => 'merchant', :email => 'tritonyelp@tritonyelp.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << u_merchant5.name
-
-s_tritonyelp = Store.create! :name => 'TritonYelp', :avg_rating => 5, :avg_price => 3.34, :x_coord => 48.89, :y_coord => 67.93, :twitter_user_name => "https://twitter.com/TritonYelp", :twitter_widget_id => "275482677962997760",
-	:description => "Class cse 112 TritonYelp project.  Yelp app just for UCSD student searching for anything UCSD.  Student can search and rate their favorite places."
-s_tritonyelp.user = u_merchant5._id
-s_tritonyelp.save!
-puts 'New store created: ' << s_tritonyelp.name
-
-
-puts 'creating stores...'
+puts 'Creating stores...'
 
 cups_store = Store.create! :name => 'Cups Coffee Cart', :avg_rating => 9.5, :avg_price => 3.34, :x_coord => 48.89, :y_coord => 67.93, 
 	:description => "Cups Coffee Cart has an assortment of excellent coffee beverages including are campus-famous Granite Bear Special. \
@@ -94,6 +110,8 @@ cups_store = Store.create! :name => 'Cups Coffee Cart', :avg_rating => 9.5, :avg
 	Come to Cups Coffe Cart today! We are conveniently located between Atkinson Hall and the Computer Science and Engineering (EBU3) building."
 cups_store.user = cups_merchant._id
 cups_store.save!
+cups_merchant.stores << cups_store
+cups_merchant.save!
 puts cups_store.name << ' created.'
 
 goodys_store = Store.create! :name => 'Goody\'s Place and Market', :avg_rating => 8.7, :avg_price => 6.55, :x_coord => 48.89, :y_coord => 67.93, 
@@ -102,6 +120,8 @@ goodys_store = Store.create! :name => 'Goody\'s Place and Market', :avg_rating =
 	outdoor walk-up window for coffee drinks. Location: Thurgood Marshall Activity Center"
 goodys_store.user = goodys_merchant._id
 goodys_store.save!
+goodys_merchant.stores << goodys_store
+goodys_merchant.save!
 puts goodys_store.name << ' created.'
 
 panda_store = Store.create! :name => 'Panda Express', :avg_rating => 6.5, :avg_price => 3.66, :x_coord => 48.89, :y_coord => 67.93, 
@@ -109,6 +129,8 @@ panda_store = Store.create! :name => 'Panda Express', :avg_rating => 6.5, :avg_p
 All of our food is MSG free hand made with your health in mind! Come visit us, we are located on the first floor of Price Center!"
 panda_store.user = panda_merchant._id
 panda_store.save!
+panda_merchant.stores << panda_store
+panda_merchant.save!
 puts panda_store.name << ' created.'
 
 bk_store = Store.create! :name => 'Burger King', :avg_rating => 5.5, :avg_price => 1.60, :x_coord => 48.89, :y_coord => 67.93, 
@@ -119,14 +141,18 @@ signature recipes, and family-friendly dining experiences is what has defined ou
 We are located on the first floor of Price Center."
 bk_store.user = bk_merchant._id
 bk_store.save!
+bk_merchant.stores << bk_store
+bk_merchant.save!
 puts bk_store.name << ' created.'
 
 subway_store = Store.create! :name => 'Subway', :avg_rating => 8.5, :avg_price => 0.05, :x_coord => 48.89, :y_coord => 67.93, 
-:description => "The Subway brand is the world's largest submarine sandwich chain with more than 37,000 locations around the world. \
+:description => "The SUBWAY brand is the world's largest submarine sandwich chain with more than 37,000 locations around the world. \
 We've become the leading choice for people seeking quick, nutritious meals that the whole family can enjoy. \
 We're located in Price Center at UCSD, come on by!"
 subway_store.user = subway_merchant._id
 subway_store.save!
+subway_merchant.stores << subway_store
+subway_merchant.save!
 puts subway_store.name << ' created.'
 
 bombay_store = Store.create! :name => 'Bombay Coast', :avg_rating => 6.5, :avg_price => 3.70, :x_coord => 48.89, :y_coord => 67.93, 
@@ -136,6 +162,8 @@ delicious Indian cuisine to our flavorful and homemade recipes - Bombay Coast br
 inside of the Price Center on the first floor."
 bombay_store.user = bombay_merchant.id
 bombay_store.save!
+bombay_merchant.stores << bombay_store
+bombay_merchant.save!
 puts bombay_store.name << ' created.'
 
 sunshine_store = Store.create! :name => 'Sunshine Market', :avg_rating => 9.5, :avg_price => 3.00, :x_coord => 48.89, :y_coord => 67.93, 
@@ -143,8 +171,9 @@ sunshine_store = Store.create! :name => 'Sunshine Market', :avg_rating => 9.5, :
 sandwiches. We are located on the first floor of Price Center."
 sunshine_store.user = sunshine_merchant._id
 sunshine_store.save!
+sunshine_merchant.stores << sunshine_store
+sunshine_merchant.save!
 puts sunshine_store.name << ' created.'
-
 
 # seeding reviews
 puts 'Creating reviews...'
@@ -163,6 +192,7 @@ cups_review_3 = Review.create! :content => 'Perfect for the on the go CS student
 cups_review_4 = Review.create! :content => 'Excellent coffee drinks - Granite Bear Special OP! Beefeater and Muffaleta sandwiches are awesome too.', 
 :user => nico_p, :store => cups_store 
 
+cups_store.reviews << cups_review_1 << cups_review_2 << cups_review_3 << cups_review_4 
 
 puts 'Creating reviews for Goodys...'
 
@@ -178,6 +208,7 @@ goodys_review_3 = Review.create! :content => 'The bowls are pretty good if your\
 goodys_review_4 = Review.create! :content => 'One of the only foods I can eat on campus without needing a bathroom within 10 meters so definiately worth 6 bucks.', 
 :user => nico_p, :store => goodys_store 
 
+goodys_store.reviews << goodys_review_1 << goodys_review_2 << goodys_review_3 << goodys_review_4 \
 
 puts 'Creating reviews for Panda Express...'
 
@@ -193,6 +224,8 @@ panda_review_3 = Review.create! :content => 'The chow mein was dry and old, defi
 panda_review_4 = Review.create! :content => 'Just ate bad orange chicken - was in the bathroom all day. Beware!!!!!!', 
 :user => nico_p, :store => panda_store 
 
+panda_store.reviews << panda_review_1 << panda_review_2 << panda_review_3 << panda_review_4 
+
 puts 'Creating reviews for Burger King...'
 
 bk_review_1 = Review.create! :content => 'A classic American establishment - you can always depend on the whopper! BK Big Fish is just as good as it always was!', 
@@ -207,6 +240,8 @@ bk_review_3 = Review.create! :content => 'Best dollar menu on campus, I always g
 bk_review_4 = Review.create! :content => 'Great for a professor on the go - speaking of great, this is by far the most robust, extensible, maintainable, testable and verifiable, modular applications I have EVER laid eyes upon. A+ jobs guys, A+ job.', 
 :user => nico_p, :store => bk_store 
 
+bk_store.reviews << bk_review_1 << bk_review_2 << bk_review_3 << bk_review_4 
+
 puts 'Creating reviews for Subway...'
 
 subway_review_1 = Review.create! :content => 'Stingy with the meat and cheese, they actually took a cheese OFF when they noticed there was one too many - RIDICULOUS!', 
@@ -215,11 +250,13 @@ subway_review_1 = Review.create! :content => 'Stingy with the meat and cheese, t
 subway_review_2 = Review.create! :content => 'Good enough when your\'e on campus, not too expensive and same Subway \'quality\'', 
 :user => josh_g, :store => subway_store 
 
-subway_review_3 = Review.create! :content => 'Wilted veggies and cheap meat, and nasty manufactured cheese - no thanks.', 
+subway_review_3 = Review.create! :content => 'Wilted veggies and cheap meat and manufactured cheese!!!! No thanks.', 
 :user => john_s, :store => subway_store 
 
 subway_review_4 = Review.create! :content => 'I\'d much rather, go to Cups or Goodys and spend a few more bucks than eat Subway and be hunugry in 30 minutes....',
 :user => nico_p, :store => subway_store 
+
+subway_store.reviews << subway_review_1 << subway_review_2 << subway_review_3 << subway_review_4
 
 puts 'Creating reviews for Bombay Coast...'
 
@@ -229,11 +266,13 @@ bombay_review_1 = Review.create! :content => 'Uuuhhh....If you\'ve ever had Indi
 bombay_review_2 = Review.create! :content => 'The panner masala tiki daal naan is really good, and pretty cheap compared to other places!', 
 :user => josh_g, :store => bombay_store 
 
-bombay_review_3 = Review.create! :content => 'Everything tastes the same, not worth the money. If I want Indian food I\'ll go to Punjabi Tandoor!!!!!!', 
+bombay_review_3 = Review.create! :content => 'Everything tastes the same, not worth the money...I doubt the authenticity of this locale!!!!	', 
 :user => cam_j, :store => bombay_store
 
 bombay_review_4 = Review.create! :content => 'The paneer masala and dal aren\'t too bad, but Punjabi Tandoor puts this place to shame.',
 :user => matt_n, :store => bombay_store 
+
+bombay_store.reviews << bombay_review_1 << bombay_review_2 << bombay_review_3 << bombay_review_4 
 
 puts 'Creating reviews for Sunshine Market...'
 
@@ -250,5 +289,115 @@ sunshine_review_3 = Review.create! :content => 'Somethings are absurdly expensiv
 sunshine_review_4 = Review.create! :content => 'One of the few markets so the prices are ridiculous, but the line moves fast and they have a decent selection.',
 :user => matt_n, :store => sunshine_store 
 
+sunshine_store.reviews << sunshine_review_1 << sunshine_review_2 << sunshine_review_3 << sunshine_review_4 
 
-# seeding tags
+
+puts 'Creating tags...'
+
+sandwich = Tag.create! :name => 'sandwich', 
+:reviews => [cups_review_1, cups_review_4, goodys_review_2, subway_review_1, subway_review_2],
+:stores => [cups_store, subway_store, goodys_store]
+
+breakfast = Tag.create! :name => 'breakfast', 
+:reviews => [cups_review_2],
+:stores => [cups_store]
+
+lunch = Tag.create! :name => 'lunch', 
+:reviews => [cups_review_2], 
+:stores => [cups_store]
+
+pizza = Tag.create! :name => 'pizza', 
+:reviews => [cups_review_2], 
+:stores => [cups_store]
+
+burrito = Tag.create! :name => 'burrito', 
+:reviews => [cups_review_2, goodys_review_1], 
+:stores => [cups_store, goodys_store]
+
+bagel = Tag.create! :name => 'bagel', 
+:reviews => [cups_review_3], 
+:stores => [cups_store]
+
+coffee = Tag.create! :name => 'coffee', 
+:reviews => [cups_review_4], 
+:stores => [cups_store]
+
+healthy = Tag.create! :name => 'healthy', 
+:reviews => [goodys_review_2, goodys_review_3, goodys_review_4],
+:stores => [goodys_store]
+
+expensive = Tag.create! :name => 'expensive', 
+:reviews => [goodys_review_3, bk_review_3, sunshine_review_3, sunshine_review_4],
+:stores => [goodys_store, bk_store, sunshine_store]
+
+water_refill = Tag.create! :name => 'water-refill', 
+:reviews => [goodys_review_4],
+:stores => [goodys_store]
+
+chicken = Tag.create! :name => 'chicken', 
+:reviews => [panda_review_1, panda_review_2, bk_review_3], 
+:stores => [panda_store, bk_store]
+
+asian = Tag.create! :name => 'asian', 
+:reviews => [panda_review_2], 
+:stores => [panda_store]
+
+gross =  Tag.create! :name => 'gross', 
+:reviews => [panda_review_3, panda_review_4, bk_review_2, subway_review_3, bombay_review_2, bombay_review_3],
+:stores => [panda_store, bk_store, subway_store, bombay_store]
+
+ibs = Tag.create! :name => 'straight-to-the-bathroom', 
+:reviews => [panda_review_4], 
+:stores => [panda_store]
+
+burger = Tag.create! :name => 'burger', 
+:reviews => [bk_review_1], 
+:stores => [bk_store]
+
+american = Tag.create! :name => 'american', 
+:reviews => [bk_review_1], 
+:stores => [bk_store]
+
+inexpensive = Tag.create! :name => 'inexpensive', 
+:reviews => [bk_review_3, subway_review_2, sunshine_review_3], 
+:stores => [bk_store, subway_store, sunshine_store]
+
+fries = Tag.create! :name => 'fries', 
+:reviews => [bk_review_3], 
+:stores => [bk_store]
+
+awesome = Tag.create! :name => 'awesome', 
+:reviews => [bk_review_4], 
+:stores => [bk_store]
+
+stingy = Tag.create! :name => 'stingy', 
+:reviews => [subway_review_1, subway_review_4],
+:stores => [subway_store]
+
+indian = Tag.create! :name => 'indian', 
+:reviews => [bombay_review_1, bombay_review_2, bombay_review_4], 
+:stores => [bombay_store]
+
+drinks = Tag.create! :name => 'drinks', 
+:reviews => [sunshine_review_1],
+:stores => [sunshine_store]
+
+groceries = Tag.create! :name => 'groceries', 
+:reviews => [sunshine_review_1], 
+:stores => [sunshine_store]
+
+soup = Tag.create! :name => 'soup', 
+:reviews => [sunshine_review_1], 
+:stores => [sunshine_store]
+
+snacks = Tag.create! :name => 'snacks', 
+:reviews => [sunshine_review_1],
+:stores => [sunshine_store]
+
+free = Tag.create! :name => 'free', 
+:reviews => [sunshine_review_2], 
+:stores => [sunshine_store]
+
+convenient = Tag.create! :name => 'convenient', 
+:reviews => [sunshine_review_4], 
+:stores => [sunshine_store]
